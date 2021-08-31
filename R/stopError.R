ff3ccf08b8f97d630789d053326e5db62d3a07d9
Adrian Dusa <dusa.adrian@gmail.com>
2021-09-01 01:47:37 +0300

    improvement on printing messages containing enter characters

--- R/stopError.R
+++ R/stopError.R
@@ -1,4 +1,29 @@
 `stopError` <- function(message, enter = "\n") {
+    
+    message <- unlist(strsplit(message, split = "\\n"))
+
+    for (i in seq(length(message))) {
+        message[i] <- gsub(
+            "Error: ",
+            ifelse(i > 1, "       ", ""),
+            paste(
+                strwrap(
+                    paste("Error:", message[i]),
+                    exdent = 7
+                ),
+                collapse = "\n"
+            )
+        )
+    }
+
     cat(enter)
-    stop(simpleError(paste0(message, enter, enter)))
+    
+    stop(
+        simpleError(
+            paste0(
+                paste(message, collapse = "\n"),
+                enter, enter
+            )
+        )
+    )
 }
