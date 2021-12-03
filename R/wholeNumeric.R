3f3a438ea2a5e2530b14e3ea938060a4ff8016da
Adrian Dusa <dusa.adrian@gmail.com>
2021-12-03 21:36:56 +0200

    rewritten function to take care of haven

--- R/wholeNumeric.R
+++ R/wholeNumeric.R
@@ -1,3 +1,28 @@
-`wholeNumeric` <- function(x) {
-    all(floor(x) == x, na.rm = TRUE)
+`wholeNumeric` <- function(x, each = FALSE) {
+    if (inherits(x, "haven_labelled") || inherits(x, "declared")) {
+        return(Recall(unclass(x), each = each))
+    }
+
+    result <- FALSE
+    isna <- is.na(x)
+    x <- asNumeric(x)
+
+    if (each) {
+        result <- rep(NA, length(x))
+        result[!isna] <- FALSE
+
+        if (any(!isna)) {
+            x <- x[!isna]
+            result[!isna & !is.na(x)] <- floor(x[!is.na(x)]) == x[!is.na(x)]
+        }
+
+        return(result)
+    }
+    else {
+        if (all(is.na(x)) || any(!isna & is.na(x))) {
+            return (FALSE)
+        }
+        
+        return(all(floor(x) == x, na.rm = TRUE))
+    }
 }
