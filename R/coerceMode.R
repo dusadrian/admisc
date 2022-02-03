ab41c4ff36aede8c57731afe3d581d8d6b6a2a13
Adrian Dusa <dusa.adrian@gmail.com>
2022-02-03 18:56:44 +0200

    documented fix and more robust function

--- R/coerceMode.R
+++ R/coerceMode.R
@@ -4,13 +4,23 @@
         stopError("The input is not atomic.")
     }
 
-    if (possibleNumeric(x) || all(is.na(x))) {
-        if (wholeNumeric(x) && is.null(tryCatchWEM(as.integer(x)))) {
-            x <- as.integer(x)
-        }
-        else {
-            x <- asNumeric(x)
-        }
+    if (
+        !is.numeric(x) && 
+        (
+            possibleNumeric(x) || all(is.na(x))
+        )
+    ) {
+        x <- asNumeric(x)
+    }
+
+    if (
+        !is.integer(x) &&
+        wholeNumeric(x) &&
+        # some whole numbers might be too big to be represented in memory
+        # as integers, in which case a warning will be captured
+        is.null(tryCatchWEM(as.integer(x)))
+    ) {
+        x <- as.integer(x)
     }
 
     return(x)
