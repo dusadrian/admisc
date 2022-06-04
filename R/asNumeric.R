ff8f15484249e2907ad55cea11574f244678f45d
Adrian Dusa <dusa.adrian@gmail.com>
2022-06-05 01:24:55 +0300

    generic function

--- R/asNumeric.R
+++ R/asNumeric.R
@@ -1,19 +1,40 @@
-`asNumeric` <- function(x) {
-    if (is.numeric(x)) {
-        return(x)
+`asNumeric` <- function(x, ...) {
+    UseMethod("asNumeric")
+}
+
+`asNumeric.declared` <- function(x, ..., na_values = TRUE) {
+    na_index <- attr(x, "na_index")
+    attributes(x) <- NULL
+
+    if (isTRUE(na_values)) {
+        if (!is.null(na_index)) {
+            # if the names are not numeric that is not a problem, NA is fine
+            x[na_index] <- as.numeric(names(na_index))
+        }
     }
 
-    if (is.factor(x)) {
+    NextMethod()
+}
+
+`asNumeric.factor` <- function(x, ..., levels = TRUE) {
+    if (isTRUE(levels)) {
         return(suppressWarnings(as.numeric(levels(x)))[x])
+    }
+    
+    return(as.numeric(x))
+}
+
+`asNumeric.default` <- function(x, ...) {
+
+    attributes(x) <- NULL
+
+    if (is.numeric(x)) {
+        return(x)
     }
 
     result <- rep(NA, length(x))
     multibyte <- grepl("[^!-~ ]", x)
 
-    if (inherits(x, "haven_labelled")) {
-        attributes(x) <- NULL
-    }
-    
     result[!multibyte] <- suppressWarnings(as.numeric(x[!multibyte]))
     
     return(result)
