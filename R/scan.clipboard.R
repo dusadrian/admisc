7d34fecdf63f04d330c39a1e73162286d78896af
Adrian Dusa <dusa.adrian@gmail.com>
2023-01-11 17:17:21 +0200

    small improvement

--- R/scan.clipboard.R
+++ R/scan.clipboard.R
@@ -3,16 +3,17 @@ scan.clipboard <- function (...) {
     
     if (Sys.info()[['sysname']] == "Darwin") {
         clipboard <- readLines(textConnection(system("pbpaste", intern = TRUE)))
-        clipboard <- clipboard[clipboard != ""]
         
         if (!is.null(dots$sep)) {
             clipboard <- unlist(strsplit(clipboard, split = dots$sep))
-        }
+        }    
     } else if (Sys.info()[['sysname']] == "Windows") {
         dots$file <- "clipboard"
         clipboard <- do.call("scan", dots)
     }
         
+    clipboard <- clipboard[clipboard != ""]
+    
     if (possibleNumeric(clipboard)) {
         return(asNumeric(clipboard))
     } else {
