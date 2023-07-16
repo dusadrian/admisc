22b529e28f6540c60b0ea315379dd278954468dd
Adrian Dușa <dusa.adrian@gmail.com>
2023-07-16 12:25:29 +0300

    double make sure with as.numeric

--- R/getLevels.R
+++ R/getLevels.R
@@ -7,11 +7,13 @@
     
     noflevels <- rep(NA, ncol(data))
     ulevels <- rep(NA, ncol(data))
-    
+
     noflevels[pN] <- apply(
         data[, pN, drop = FALSE],
         2,
-        max
+        # as.numeric() still makes sense, for instance as.factor(c(0,1,1,0,1))
+        # which IS possibly numeric, yet still doesn't cope with max()
+        function(x) max(as.numeric(x))
     ) + 1
 
     ulevels <- apply(
@@ -24,22 +26,22 @@
 
     noflevels[is.na(noflevels)] <- ulevels[is.na(noflevels)]
 
-    
+
     factor <- unlist(lapply(data, is.factor))
     declared <- unlist(lapply(data, function(x) inherits(x, "declared")))
-    
+
     noflevels[pN][
         apply(
             data[, pN, drop = FALSE],
             2,
-            function(x) any(x %% 1 > 0)
+            function(x) any(as.numeric(x) %% 1 > 0)
         )
     ] <- 2
-    
+
     if (any(factor | declared)) {
         noflevels[factor | declared] <- pmin(noflevels[factor | declared], ulevels[factor | declared])
     }
-    
+
     noflevels[noflevels == 1] <- 2
     return(noflevels)
 }
