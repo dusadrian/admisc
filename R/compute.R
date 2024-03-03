d09e686299ae5107bd9f6e24ded7ffcb1a91cf0a
Adrian Dușa <dusa.adrian@gmail.com>
2024-03-03 10:42:34 +0200

    eval.parent() instead of get()

--- R/compute.R
+++ R/compute.R
@@ -1,6 +1,6 @@
 `compute` <-
 function(expression = "", data = NULL, separate = FALSE, ...) { # na.rm = FALSE
-    
+
     expression <- recreate(substitute(expression))
     if (grepl("<-|<=|=>|->", expression)) {
         stopError("This function is not intended to calculate parameters of fit.")
@@ -13,46 +13,48 @@ function(expression = "", data = NULL, separate = FALSE, ...) { # na.rm = FALSE
     ) {
         expression <- paste0("1-", substring(expression, 3, enchar - 1))
     }
-    
+
     negated <- identical(unname(substring(expression, 1, 2)), "1-")
     expression <- gsub("1-", "", expression)
-    
+
     if (is.null(data)) {
         syscalls <- as.character(sys.calls())
         usingwith <- "admisc::using\\(|using\\(|with\\("
         if (any(usingdata <- grepl(usingwith, syscalls))) {
-            data <- get(
-                unlist(strsplit(gsub(usingwith, "", syscalls), split = ","))[1],
-                envir = length(syscalls) - tail(which(usingdata), 1)
-            )
+            dataname <- unlist(strsplit(gsub(usingwith, "", syscalls), split = ","))[1]
+            data <- eval.parent(parse(text = dataname, n = 1))
+            # data <- get(
+            #     dataname,
+            #     envir = length(syscalls) - tail(which(usingdata), 1)
+            # )
         }
         else {
-            
+
             colnms <- colnames(
                 validateNames(
                     notilde(expression),
                     sort(eval.parent(parse(text = "ls()", n = 1)))
                 )
             )
-            
+
             data <- vector(mode = "list", length = length(colnms))
             for (i in seq(length(data))) {
                 data[[i]] <- eval.parent(
                     parse(text = sprintf("get(\"%s\")", colnms[i]), n = 1)
                 )
             }
-            
+
             # TODO: ?model.frame
 
             if (length(unique(unlist(lapply(data, length)))) > 1) {
                 stopError("Objects should be vectors of the same length.")
             }
-            
+
             names(data) <- colnms
             data <- as.data.frame(data)
         }
     }
-    
+
     multivalue <- grepl("\\{|\\}|\\[|\\]", expression)
 
     if (!multivalue) {
@@ -69,60 +71,60 @@ function(expression = "", data = NULL, separate = FALSE, ...) { # na.rm = FALSE
     pp <- lapply(pp, function(x) x[retain])
     ppm <- ppm[, retain, drop = FALSE]
     data <- data[, retain, drop = FALSE]
-    
+
     infodata <- getInfo(data)
-    
+
     data <- infodata$data
-    
+
     verify(data)
-    
+
     tempList <- vector("list", length(pp))
 
     for (i in seq(length(pp))) {
-        
+
         x <- which(ppm[i, ] >= 0)
         val <- pp[[i]][x]
-        
+
         temp <- data[, colnames(ppm)[x], drop = FALSE]
-        
+
         for (j in seq(length(val))) {
-            
+
             if (!is.numeric(temp[, j]) & possibleNumeric(temp[, j])) {
                 temp[, j] <- asNumeric(temp[, j])
             }
-            
+
             nao <- na.omit(temp[, j])
 
             if (any(abs(nao - round(nao)) >= .Machine$double.eps^0.5)) { # fuzzy
-                
+
                 if (length(val[[j]]) > 1) {
                     stopError("Multiple values specified for fuzzy data.")
                 }
-                
+
                 if (val[[j]] == 0) {
                     temp[, j] <- 1 - temp[, j]
                 }
-        
+
             }
             else { # cs or mv
-                
+
                 temp[, j] <- as.numeric(is.element(temp[, j], val[[j]]))
-                
+
             }
-            
+
         }
-        
+
         if (ncol(temp) > 1) {
             # temp <- as.vector(fuzzyand(temp))
             temp <- apply(temp, 1, min, na.rm = FALSE)
         }
-        
+
         tempList[[i]] <- temp
     }
-    
+
     res <- as.data.frame(matrix(unlist(tempList), ncol = length(tempList)))
     colnames(res) <- rownames(ppm)
-    
+
     if (ncol(res) > 1) {
         if (!separate) {
             # res <- as.vector(fuzzyor(res))
@@ -132,8 +134,8 @@ function(expression = "", data = NULL, separate = FALSE, ...) { # na.rm = FALSE
     else {
         res <- as.vector(res[, 1])
     }
-    
+
     if (negated) res <- 1 - res
-    
+
     return(res)
 }
