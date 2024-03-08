6a5122d8a024772d1187dde7c90de4475e2ce82b
Adrian Dușa <dusa.adrian@gmail.com>
2024-03-08 19:35:58 +0200

    bug fix for truth tables

--- R/change.R
+++ R/change.R
@@ -8,14 +8,11 @@
 }
 
 `change.QCA_tt` <- function(x, ...) {
-    
     metacall <- match.call(expand.dots = TRUE)
     callargs <- as.list(metacall[-1])
 
-    dots <- list(...)
-
     if (!requireNamespace("QCA", quietly = TRUE)) {
-        enter <- ifelse(isFALSE(dots$enter), "", "\n") # internal
+        enter <- ifelse(isFALSE(callargs$enter), "", "\n") # internal
         message(
             paste(
                 enter,
@@ -37,29 +34,36 @@
     if (length(callargs) == 1 & length(nullnms) == 0) {
         return(x) # nothing to do
     }
-    
-    calls <- sapply(callargs, is.call)
 
-    if (any(calls)) {
-        for (i in which(calls)) {
-            callist <- as.list(callargs[[i]])
-            if (as.character(callist[[1]]) == "findRows") {
-                if (is.null(callist$obj)) {
-                    callist$obj <- callargs$x
-                    callargs[[i]] <- as.call(callist)
-                }
+    object <- callargs[["x"]]
+
+    `modify` <- function(x) {
+        calls <- sapply(x, is.call)
+        if (any(calls)) {
+            for (i in which(calls)) {
+                x[[i]] <- as.call(Recall(as.list(x[[i]])))
             }
         }
+
+        if (as.character(x[[1]]) == "findRows") {
+            if (is.null(x$obj)) {
+                x$obj <- object
+            }
+        }
+
+        return(x)
     }
 
+    callargs <- modify(callargs)
 
-    callist <- as.list(x$call)
+    # x is a truth table (an object of class "QCA_tt")
+    callist <- as.list(x$call) # therefore it has a "call" component
     ttname <- as.character(callargs[["x"]])
 
     for (i in seq(2, length(callist))) {
         callist[[i]] <- admisc::recreate(callist[[i]])
     }
-    
+
     callist$data <- x$initial.data
 
     if (length(callargs) > 1) {
@@ -77,11 +81,11 @@
             callist[[nm]] <- NULL
         }
     }
-    
+
     x <- do.call("truthTable", callist[-1])
     callist$data <- ttname
 
     x$call <- as.call(callist)
-    
+
     return(x)
 }
