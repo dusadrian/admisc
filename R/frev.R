aa2f7402e66f9cff694d131c9e53cba766a13667
Adrian Dușa <dusa.adrian@gmail.com>
2025-03-13 12:15:43 +0200

    finvert() renamed to frev()

--- R/frev.R
+++ R/frev.R
@@ -1,8 +1,13 @@
-`finvert` <- function(x, levels = FALSE) {
+`frev` <- function(x, levels = FALSE) {
     # to do, same for haven_labelled and declared
     if (!is.factor(x)) {
         stopError("The variable is not a factor.")
     }
     flist <- list(levels(x), rev(levels(x)))
     return(factor(x, levels = flist[[1 + !levels]], labels = flist[[1 + levels]]))
+}
+
+`finvert` <- function(...) {
+    .Deprecated(msg = "Function finvert() is deprecated, use frev().\n")
+    frev(...)
 }
